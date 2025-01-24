41c327adc0621de2d14d0f77a8d262dc100ea438
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2025-01-24 15:13:55 -0300

    feat: implementa validações na PostProductDTO para criar um novo produto

--- src/main/java/com/example/retornosAPI/dtos/PostProductDTO.java
+++ src/main/java/com/example/retornosAPI/dtos/PostProductDTO.java
@@ -1,12 +1,28 @@
 package com.example.retornosAPI.dtos;
 
 import com.example.retornosAPI.models.ProductEntity;
+import jakarta.validation.constraints.*;
 
-public record PostProductDTO(Long id, String name, Double price, int stock, String category) {
+public record PostProductDTO(
+        @NotBlank(message = "O nome não pode ser vazio")
+        @NotNull(message = "O nome não pode ser nulo")
+        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+        String name,
+        @NotBlank(message = "O preço não pode ser vazio")
+        @NotNull(message = "O preço não pode ser nulo")
+        @DecimalMin(value = "0.01", inclusive = true, message = "O preço deve ser maior que zero")
+        Double price,
+        @NotBlank(message = "O estoque não pode ser vazio")
+        @NotNull(message = "O estoque não pode ser nulo")
+        @Min(value = 0, message = "O estoque deve ser um número inteiro maior ou igual a zero")
+        int stock,
+        @NotBlank(message = "A categoria não pode ser vazia")
+        @NotNull(message = "A categoria não pode ser nula")
+        @Pattern(regexp = "Eletrônicos|Roupas|Alimentos", message = "A categoria deve ser Eletrônicos, Roupas ou Alimentos")
+        String category) {
 
     public ProductEntity dtoToEntity() {
         ProductEntity newProduct = new ProductEntity();
-        newProduct.setId(id);
         newProduct.setName(name);
         newProduct.setPrice(price);
         newProduct.setStock(stock);
